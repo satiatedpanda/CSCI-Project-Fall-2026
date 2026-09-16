218a5168f44126bf4799ef9ce78841dc223dd068
satiatedpanda <124468098+satiatedpanda@users.noreply.github.com>
2026-09-16 12:21:53 -0500

    Add files via upload

---
+++ blackjack.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*  Figure out how to randomize an array
+    How Combine and shuffle the array
+    How to store bets
+    https://bicyclecards.com/how-to-play/blackjack
+*/
+
+int main(void) {
+    int Deck[] = {};
+    srand(time(NULL));
+    
+    // You get two cards -> dealer gets 1 face up 1 face down card -> you chose your stuff -> dealer does stuff -> reward -> repeat
+
+
+
+}
