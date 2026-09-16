376c35480cfae67230f8af3bcaf6492a718d90d4
satiatedpanda <124468098+satiatedpanda@users.noreply.github.com>
2026-09-16 12:28:04 -0500

    Add files via upload

--- blackjack.c
+++ blackjack.c
@@ -10,11 +10,13 @@
 */
 
 int main(void) {
-    int Deck[] = {};
+    int CardDeck[] = {};
     srand(time(NULL));
     
     // You get two cards -> dealer gets 1 face up 1 face down card -> you chose your stuff -> dealer does stuff -> reward -> repeat
 
-
+    // Devin : figure out how to generate a card and randomize a deck to get 5 decks
+    // Kevin : figure out how to bet and store bids, while having a set amount to gamble // reward system
+    // Jacob : figure out the logic for the dealer and to show the cards delt
 
 }
