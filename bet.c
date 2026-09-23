c81abb511612207c991678a734fcad8004826472
kevsx621 <kevpare97@gmail.com>
2026-09-22 19:29:03 -0500

    Add files via upload
    Finalized bet system

--- bet.c
+++ bet.c
@@ -8,6 +8,7 @@ int main(void) {
 
     int balance; // the initial amount of cents 10000 = 100$
     int bet;     // holds bet value
+    int result;  // results from win/losses
 
     printf("Enter starting balance: \n"); // User enters betting amount
     if (scanf("%d", &balance) != 1) { // !=1 assures 1 integer value is entered
@@ -31,21 +32,58 @@ int main(void) {
             break;      // Exits loop. Skips else if statements and prints final
                         // balance
         }
-        // if bet is false. The following statements are considered
+        // if bet is not == 0. The following statements are considered
         else if (bet < 0) {
-            printf("You do not have enough mone\n");
+            printf("You do not have enough money\n");
         } else if (bet > balance) {
-            printf("You can't place a negative bet\n");
+            printf("You do not have enough money\n");
         }
         // Once the previous statements have been verified.
         // The following statement continues
         else {
-            balance = balance - bet;
-            printf("You placed a bet of %d.\n", bet);
+            balance = balance - bet; // New balance value substracting from bet
+            printf("You placed a bet of $%d.\n", bet); // Shows the bet entered
+
+            printf(
+                "Enter result: 1 = win, 2 = loss, 3 = tie: "
+            ); // User will input end results based off game decisions
+
+            if (scanf("%d", &result) !=
+                1) { // Results gets assigned to int result
+
+                return 1; // Stops programs when fails to read input
+            }
+            if (result ==
+                1) { // If result = 1 the following statement is processed
+                balance = balance + bet * 2;
+                printf("You won!\n");
+            }
+
+            else if (result == 2) { // if the result = 2 the following statement
+                                    // is processed
+                printf("You lost!\n");
+            }
+
+            else if (result == 3) { // if the result = 3 the following state is
+                                    // processed
+                balance = balance + bet;
+                printf("It's a tie. Your bet was returned\n");
+            } else {
+                balance =
+                    balance + bet; // if input is anything else besides the
+                                   // options. Invalid message will prompt.
+                printf("invalid result. Your bet has been returned.\n");
+            }
+
+            printf(
+                "Your updated balance: $%d\n", balance
+            ); // Updated balance after wins/losses
         }
     }
 
-    printf("Your final balance is $%d.\n", balance);
+    printf(
+        "Your final balance is $%d.\n", balance
+    ); // Final balance after game exits
 
     return 0;
 }
